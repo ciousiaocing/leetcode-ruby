1af681c1fdf163cea724cf60fc35804b68c97ec7
ciou <yyouko22@gmail.com>
2018-01-01 00:34:11 +0800

    each_cons method

--- algorithms/roman_to_integer.rb
+++ algorithms/roman_to_integer.rb
@@ -25,6 +25,17 @@ def roman_to_int(s)
     sum
 end
 
+# (1..10).each_cons(3) { |a| p a }
+# # outputs below
+# [1, 2, 3]
+# [2, 3, 4]
+# [3, 4, 5]
+# [4, 5, 6]
+# [5, 6, 7]
+# [6, 7, 8]
+# [7, 8, 9]
+# [8, 9, 10]
+
 p roman_to_int('XI')
 p roman_to_int('IX')
 p roman_to_int('DCXXI')
